f860dc88764c70af8e4771c63a54eac0f91df5f6
Aliaksei Raketski <raketskij@gmail.com>
2019-08-06 21:56:25 +0300

    fix: зкудщфвук

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-$(window).on('load', function () {
-  var $preloader = $('.preloader_container')
-  $preloader.delay(500).fadeOut('slow');
-})
-
 $(window).scroll(function(){
   var sticky = $('.sticky'),
       scroll = $(window).scrollTop();
